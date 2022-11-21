1faa6c8444b990815b25ba5bc453d78e1a6c7c78
stiankodehode <stiankodehode@gmail.com>
2022-11-21 10:52:47 +0100

    changed json files to contain data from vegvesen api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className="App">
             <Form />
-             <FetchData  />
+            <FetchData />
         </div>
     );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-import Counties from "../data/json/county.json";
-import Municipalities from "../data/json/municipality.json";
+import counties from "../data/json/counties.json";
+import municipalities from "../data/json/municipalities.json";
 
 import { downloadIcon } from "../img/svg";
 import {
@@ -25,30 +25,31 @@ const Form = () => {
     });
 
     // Sorting the counties from the Counties JSON.
-    const sortedCounties = Counties.features.sort((a, b) => {
-        const nameA = a.properties.navn[0].navn.toUpperCase();
-        const nameB = b.properties.navn[0].navn.toUpperCase();
+    const sortedCounties = counties.sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
         return nameA > nameB ? 1 : -1;
     });
     // Mapping the counties as options for a datalist.
     const countiesList = sortedCounties.map((county, idx) => {
-        return <option key={idx} value={county.properties.navn[0].navn} />;
+        return <option key={idx} value={county.name} />;
     });
 
     // Sorting the municipalities from the Municipalities JSON.
-    const sortedMuncipalities = Municipalities.features.sort((a, b) => {
-        const nameA = a.properties.navn[0].navn.toUpperCase();
-        const nameB = b.properties.navn[0].navn.toUpperCase();
+    const sortedMuncipalities = municipalities.sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
         return nameA > nameB ? 1 : -1;
     });
 
     // Mapping the municipalities as option for a datalist.
     const municipalityList = sortedMuncipalities.map((municipality, idx) => {
-        return <option key={idx} value={municipality.properties.navn[0].navn} />;
+        return <option key={idx} value={municipality.name} />;
     });
 
     // Updates form state with new values on change
     const handleChange = (event) => {
+        console.log(event.target);
         const { name, value } = event.target;
         setFormInfo((oldForm) => ({
             ...oldForm,
