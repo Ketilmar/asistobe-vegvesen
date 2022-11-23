a209be4fc61d4de8cc57c991ab8536014ab390f3
Sofie <sofie.kodehode@gmail.com>
2022-11-23 13:26:21 +0100

    working csv

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,131 +3,122 @@ import { useState } from "react";
 import counties from "../data/json/counties.json";
 import municipalities from "../data/json/municipalities.json";
 
-import { downloadIcon } from "../img/svg";
 import {
-    StyledInput,
-    StyledForm,
-    StyledLabel,
-    FlexRow,
-    FlexColumn,
-    StyledDatalist,
-    StyledInputDate,
-    StyledButton,
-    ButtonLink,
+  StyledInput,
+  StyledForm,
+  StyledLabel,
+  FlexRow,
+  FlexColumn,
+  StyledDatalist,
 } from "./styled";
 
 const Form = () => {
-    const [formInfo, setFormInfo] = useState({
-        county: { name: "", number: undefined },
-        municipality: "",
-        dateFrom: "",
-        dateTo: "",
+  const [formInfo, setFormInfo] = useState({
+    county: { name: "", number: undefined },
+    municipality: "",
+    dateFrom: "",
+    dateTo: "",
+  });
+
+  // Sorting the counties from the Counties JSON.
+  const sortedCounties = counties.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    return nameA > nameB ? 1 : -1;
+  });
+  // Mapping the counties as options for a datalist.
+  const countiesList = sortedCounties.map((county, idx) => {
+    return <option key={idx} value={county.name} />;
+  });
+
+  // Sorting the municipalities from the Municipalities JSON.
+  const sortedMuncipalities = municipalities.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    return nameA > nameB ? 1 : -1;
+  });
+
+  // Mapping the municipalities as option for a datalist.
+  const municipalityList = sortedMuncipalities.map((municipality, idx) => {
+    return <option key={idx} value={municipality.name} />;
+  });
+
+  // Updates form state with new values on change
+  const handleChange = (event) => {
+    // The name and value of the target
+    const { name, value } = event.target;
+    // If the selected counties is in the counties find the info.
+    const county = counties.find((county) => {
+      return county.name === value;
     });
+    // sets the form info to contain name and number
+    if (county) {
+      setFormInfo((oldForm) => ({
+        ...oldForm,
+        county: { name: county.name, number: county.number },
+      }));
+      return;
+    }
+    // Handles everything but the county
+    setFormInfo((oldForm) => ({
+      ...oldForm,
+      [name]: value,
+    }));
+  };
 
-    // Sorting the counties from the Counties JSON.
-    const sortedCounties = counties.sort((a, b) => {
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-        return nameA > nameB ? 1 : -1;
-    });
-    // Mapping the counties as options for a datalist.
-    const countiesList = sortedCounties.map((county, idx) => {
-        return <option key={idx} value={county.name} />;
-    });
+  // Handles the submit, currently just console logs the state object.
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formInfo);
+  };
 
-    // Sorting the municipalities from the Municipalities JSON.
-    const sortedMuncipalities = municipalities.sort((a, b) => {
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-        return nameA > nameB ? 1 : -1;
-    });
+  return (
+    <StyledForm>
+      <h2>County/Municipality</h2>
+      <FlexRow>
+        <FlexColumn>
+          <StyledLabel>County:</StyledLabel>
+          <StyledInput
+            list="county-list"
+            name="county"
+            onChange={handleChange}
+          ></StyledInput>
+        </FlexColumn>
+        <FlexColumn>
+          <StyledLabel>Municipality:</StyledLabel>
+          <StyledInput
+            list="municipality-list"
+            name="municipality"
+            onChange={handleChange}
+          ></StyledInput>
+        </FlexColumn>
+      </FlexRow>
 
-    // Mapping the municipalities as option for a datalist.
-    const municipalityList = sortedMuncipalities.map((municipality, idx) => {
-        return <option key={idx} value={municipality.name} />;
-    });
+      <h2>Date</h2>
+      <FlexRow>
+        <FlexColumn>
+          <StyledLabel>From:</StyledLabel>
+          <StyledInput
+            type="datetime-local"
+            name="dateFrom"
+            onChange={handleChange}
+          ></StyledInput>
+        </FlexColumn>
+        <FlexColumn>
+          <StyledLabel>To:</StyledLabel>
+          <StyledInput
+            type="datetime-local"
+            name="dateTo"
+            onChange={handleChange}
+          ></StyledInput>
+        </FlexColumn>
+      </FlexRow>
 
-    // Updates form state with new values on change
-    const handleChange = (event) => {
-        // The name and value of the target
-        const { name, value } = event.target;
-        // If the selected counties is in the counties find the info.
-        const county = counties.find((county) => {
-            return county.name === value;
-        });
-        // sets the form info to contain name and number
-        if (county) {
-            setFormInfo((oldForm) => ({
-                ...oldForm,
-                county: { name: county.name, number: county.number },
-            }));
-            return;
-        }
-        // Handles everything but the county
-        setFormInfo((oldForm) => ({
-            ...oldForm,
-            [name]: value,
-        }));
-    };
-
-    // Handles the submit, currently just console logs the state object.
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(formInfo);
-    };
-
-    return (
-        <StyledForm>
-            <h2>County/Municipality</h2>
-            <FlexRow>
-                <FlexColumn>
-                    <StyledLabel>County:</StyledLabel>
-                    <StyledInput
-                        list="county-list"
-                        name="county"
-                        onChange={handleChange}
-                    ></StyledInput>
-                </FlexColumn>
-                <FlexColumn>
-                    <StyledLabel>Municipality:</StyledLabel>
-                    <StyledInput
-                        list="municipality-list"
-                        name="municipality"
-                        onChange={handleChange}
-                    ></StyledInput>
-                </FlexColumn>
-            </FlexRow>
-
-            <h2>Date</h2>
-            <FlexRow>
-                <FlexColumn>
-                    <StyledLabel>From:</StyledLabel>
-                    <StyledInput
-                        type="datetime-local"
-                        name="dateFrom"
-                        onChange={handleChange}
-                    ></StyledInput>
-                </FlexColumn>
-                <FlexColumn>
-                    <StyledLabel>To:</StyledLabel>
-                    <StyledInput
-                        type="datetime-local"
-                        name="dateTo"
-                        onChange={handleChange}
-                    ></StyledInput>
-                </FlexColumn>
-            </FlexRow>
-
-            {/* Data Lists */}
-            <StyledDatalist id="county-list">{countiesList}</StyledDatalist>
-            <StyledDatalist id="municipality-list">{municipalityList}</StyledDatalist>
-
-            <StyledButton onClick={handleSubmit}>
-                <ButtonLink>Export as CSV</ButtonLink>
-                {downloadIcon}
-            </StyledButton>
-        </StyledForm>
-    );
+      {/* Data Lists */}
+      <StyledDatalist id="county-list">{countiesList}</StyledDatalist>
+      <StyledDatalist id="municipality-list">{municipalityList}</StyledDatalist>
+    </StyledForm>
+  );
 };
 
 export default Form;
--- src/components/fetchData.js
+++ src/components/fetchData.js
@@ -1,51 +1,90 @@
 import { useEffect, useState } from "react";
+import { CSVLink } from "react-csv";
+import { downloadIcon } from "../img/svg";
+import { ButtonLink, StyledButton } from "./styled";
 
 const FetchData = () => {
   const [data, setData] = useState(null);
 
-  const httpOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    mode: "cors",
-    body: JSON.stringify({
-      query: `{
-        trafficRegistrationPoints {
-          location {
-            county {
-              name
-              number
-            }
-            municipality {
-              name
-              number
+  useEffect(() => {
+    const httpOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      body: JSON.stringify({
+        query: ` {
+    
+           trafficRegistrationPoints {
+            location {
+              county {
+                name
+                number
+              }
+              municipality {
+                name
+                number
+              }
             }
           }
-        }
-      }`,
-    }),
-  };
+        }`,
+        variables: null,
+      }),
+    };
 
-  const fetchApi = async () => {
-       await fetch("https://www.vegvesen.no/trafikkdata/api/", httpOptions)
-      .then((res) => res.json())
-      .then((res) => setData(res))
-      .catch((err) => console.log(err))
-      // .finally()
-  };
+    const cleanData = (data) => {
+      const cleanedData = data.data.trafficRegistrationPoints.map((item) => ({
+        countryName: item.location.county.name,
+        countryNumber: item.location.county.number,
+        municipalityName: item.location.municipality.name,
+        municipalityNumber: item.location.municipality.number,
+      }));
+      return cleanedData;
+    };
 
-  useEffect(() => {
+    const fetchApi = async () => {
+      await fetch("https://www.vegvesen.no/trafikkdata/api/", httpOptions)
+        .then((res) => res.json())
+        .then((res) => {
+          const cleanedData = cleanData(res);
+          setData(cleanedData);
+        })
+        .catch((err) => console.log(err));
+    };
+
     fetchApi();
-
-  }, [])
+  }, []);
 
   console.log(data);
 
-  
+  const columns = [
+    {
+      countryName: "countryName",
+      countryNumber: "countryNumber",
+    },
+    {
+      countryName: "countryName",
+      countryNumber: "countryNumber",
+    },
+  ];
+
   return (
     <>
-    {JSON.stringify(data)}
+      {data && (
+        <CSVLink
+          data={data}
+          separator={";"}
+          columns={columns}
+          filename={"vegvesenet.csv"}
+        >
+          <StyledButton>
+            <ButtonLink>Export as CSV</ButtonLink>
+            {downloadIcon}
+          </StyledButton>
+        </CSVLink>
+      )}
+      ;
     </>
-  )
+  );
 };
 
-export {FetchData}
+export { FetchData };
