deba03f67d1e8a972b114803bbf8573f1a84d1c6
michhkh <michaelh.kodehode@gmail.com>
2023-01-09 13:10:07 +0100

    Updated headers to be byDay

--- src/components/regpointDataCsv.js
+++ src/components/regpointDataCsv.js
@@ -6,17 +6,17 @@ const RegpointDataCsv = (data) => {
     // TEST: how to get csv headers
     const header1 = (Object.keys(data.data.trafficData.trafficRegistrationPoint));
     // const header2 = (Object.keys(data.data.trafficData.volume));
-    const header3 = (Object.keys(data.data.trafficData.volume.byHour.edges[0].node));
-    const header4 = (Object.keys(data.data.trafficData.volume.byHour.edges[0].node.total));
-    const header5 = (Object.keys(data.data.trafficData.volume.byHour.edges[0].node.byLengthRange[0].lengthRange));
+    const header3 = (Object.keys(data.data.trafficData.volume.byDay.edges[0].node));
+    const header4 = (Object.keys(data.data.trafficData.volume.byDay.edges[0].node.total));
+    const header5 = (Object.keys(data.data.trafficData.volume.byDay.edges[0].node.byLengthRange[0].lengthRange));
 
     // combine all the headers
     const header = header1.concat(header3, header4, header5)
     
     console.table(data.data.trafficData.trafficRegistrationPoint);
-    console.table(data.data.trafficData.volume.byHour.edges[0].node);
-    console.table(data.data.trafficData.volume.byHour.edges[0].node.total);
-    console.table(data.data.trafficData.volume.byHour.edges[0].node.byLengthRange[0].lengthRange);
+    console.table(data.data.trafficData.volume.byDay.edges[0].node);
+    console.table(data.data.trafficData.volume.byDay.edges[0].node.total);
+    console.table(data.data.trafficData.volume.byDay.edges[0].node.byLengthRange[0].lengthRange);
 }
 
 export {RegpointDataCsv}
