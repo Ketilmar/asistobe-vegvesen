27fa6e7977d9c7b00eb7ed87ce894adf0105c554
michhkh <michaelh.kodehode@gmail.com>
2023-01-12 15:54:56 +0100

    cleaned up code

--- src/components/regpointDataCsv.js
+++ src/components/regpointDataCsv.js
@@ -4,14 +4,14 @@ const RegpointDataCsv = (data) => {
 
     console.table(data);
     // TEST: how to get csv headers
-    
+
     const header1 = (Object.keys(data.data.trafficData.trafficRegistrationPoint));
     // const header2 = (Object.keys(data.data.trafficData.volume));
     // const header3 = (Object.keys(data.data.trafficData.volume.byHour.edges[10].node));
     // const header4 = (Object.keys(data.data.trafficData.volume.byHour.edges[10].node.total));
     //const header5 = (Object.keys(data.data.trafficData.volume.byHour.edges[10].node.byLengthRange[0].lengthRange));
     const header6 = (Object.keys(data.data.trafficData.trafficRegistrationPoint.location.coordinates.latLon));
-    const pathToEdges = data.data.trafficData.volume.byHour.edges
+    const pathToEdges = data.data.trafficData.volume.byHour.edges;
 
     const edgeData = (arr = []) => {
         const result = arr.reduce((acc, obj, index) => {
@@ -23,14 +23,14 @@ const RegpointDataCsv = (data) => {
         return result;
     };
 
-    const headerEdges = (Object.keys(edgeData(pathToEdges)))
+    const headerEdges = (Object.keys(edgeData(pathToEdges)));
 
 
     // combine all the headers
     //const header = header1.concat(header3, header4, header5)
     
     console.table(data.data.trafficData.trafficRegistrationPoint);
-    console.table(edgeData(pathToEdges))
+    console.table(edgeData(pathToEdges));
     //console.table(data.data.trafficData.volume.byHour.edges[10].node);
     //console.table(data.data.trafficData.volume.byHour.edges[10].node.total);
     //console.table(data.data.trafficData.volume.byHour.edges[10].node.byLengthRange[0].lengthRange);
