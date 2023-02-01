c023438a3f55b4c93b623fb206af52c1f627ab23
michhkh <michaelh.kodehode@gmail.com>
2023-02-01 13:33:17 +0100

    updated getDefaultDates to work with month changes and leap years

--- src/components/getDefaultDates.js
+++ src/components/getDefaultDates.js
@@ -1,25 +1,8 @@
+import moment from "moment/moment.js";
+
 const getDefaultDates = (daysToGoBack) => {
-  const getDay = new Date().getDate();
-  const getMonth = new Date().getMonth() + 1;
-  const year = new Date().getFullYear();
-
-  //Takes in a number below 10 and returns a 0 in front of that number
-  //Example:
-  //1 -> 01
-  //9 -> 09
-  const setDoubleDigits = (num) => {
-    if (num < 10) {
-      return `${0}${num}`;
-    } else {
-      return num;
-    }
-  };
-
-  // const day = setDoubleDigits(getDate)
-  const month = setDoubleDigits(getMonth);
-  const day = setDoubleDigits(getDay - daysToGoBack);
-
-  return `${year}-${month}-${day}`;
+  const date = moment().subtract(daysToGoBack, "days").format("YYYY-MM-DD")
+  return date
 };
 
 const fromDateDefault = getDefaultDates(2);
