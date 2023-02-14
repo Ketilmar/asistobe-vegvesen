459c77714d22f0b8ca985e307ed610f2b0bce766
ketilmar <ketilmar.kodehode@gmail.com>
2023-02-14 10:42:57 +0100

    Fixed getValue for testing

--- src/components/trafficVolumeByLengthCsv.js
+++ src/components/trafficVolumeByLengthCsv.js
@@ -2,76 +2,90 @@ import { FileWriter } from "./fileWriter.js";
 import fs from 'fs';
 
 
-const tmpRowData = new Array();
-const rowData = new Array();
+const outerFunc = (data) => {
 
-/** Iterates thru the input object one level each loop and extract all values. Pushes values to rowData as an array of arrays. Values stored as string */
-const getValues = (data) => {
-  
-  // If not an object, get the value and push it to tmpRowData array
-  if(typeof data !== 'object'){
-    tmpRowData.push(data.toString().replace(/[\,]+/g, ' -'))
-    return;
-  }
+  const tmpRowData = new Array();
+  const rowData = new Array();
 
-  // Handles empty data
-  if (data === null){return};
+  /** Iterates thru the input object one level each loop and extract all values. Pushes values to rowData as an array of arrays. Values stored as string */
+  const getValues = (data) => {
 
-  // Handles objects with no 'edge' data
-  if (Object.keys(data).includes('edges')){
-    if (data.edges.length === 0){
-      rowData.push(tmpRowData.splice(0,7));
-      rowData.push(['No data'])
+    console.log({data});
+    // If not an object, get the value and push it to tmpRowData array
+    if(typeof data !== 'object'){
+      tmpRowData.push(data.toString().replace(/[\,]+/g, ' -'))
+      return;
     }
-  }
-  
-  // when loop gets to 'byLengthRange', it takes the data collected in tmpRowData and stores it in rowData as an array
-  // updates rowData array with 'idInfo' at index 0, then volume data for each time range in their respective index
-  // First loop contains 'idInfo' data, so we account for that with '+ 6'. (could maybe use a high 'deleteCount' number instead of using 'byLengthRange' length)
-  if (Object.keys(data).includes('byLengthRange')){
-      rowData.push(tmpRowData.splice(0, data.byLengthRange.length + 7));
+
+    // Handles empty data
+    if (data === null){return};
+
+    // Handles objects with no 'edge' data
+    if (Object.keys(data).includes('edges')){
+      if (data.edges.length === 0){
+        rowData.push(tmpRowData.splice(0,7));
+        rowData.push(['No data'])
+      }
+    }
+    
+    // when loop gets to 'byLengthRange', it takes the data collected in tmpRowData and stores it in rowData as an array
+    // updates rowData array with 'idInfo' at index 0, then volume data for each time range in their respective index
+    // First loop contains 'idInfo' data, so we account for that with '+ 7'. (could maybe use a high 'deleteCount' number instead of using 'byLengthRange' length)
+    if (Object.keys(data).includes('byLengthRange')){
+        rowData.push(tmpRowData.splice(0, data.byLengthRange.length + 7));
+    };
+
+    // each loop it takes the data object and flattens it to a new array. Then send the new array thru getValues() again. 
+    Object.values(data).flatMap(v => getValues(v));
+
+    // the last node (time period) must be pushed here not to loose it.
+    // Checking for content in tmpRowData to avoid pushing empty arrays (mostly for 'no edge' data)
+    if (Object.keys(data).includes('data')){
+      if(tmpRowData.length !== 0){
+        rowData.push(tmpRowData.splice(0, tmpRowData.length))
+      }
+    };
+    
+    return rowData;
   };
 
-  // each loop it takes the data object and flattens it to a new array. Then send the new array thru getValues() again. 
-  Object.values(data).flatMap(v => getValues(v));
-
-  // console.log({rowData});
-  return rowData;
+return getValues(data);
 };
 
 
 /** Parses data object from 'trafficVolumeByLength' query. Converts to csv and sends object to FileWriter() */
 const TrafficVolumeByLengthCsv = (data, callback) => {
-  
-  getValues(data)
+  let returnedRowData;
+
+  returnedRowData = outerFunc(data);
 
   // extract data for adding to each row later (id, name, county, municipality, lat, lon)
-  let idInfo = rowData.shift();
+  let idInfo = returnedRowData.shift();
 
   // put in 'idInfo' in each row
-  for (let item in rowData){
-    rowData[item].unshift(idInfo.toString())
+  for (let item in returnedRowData){
+    returnedRowData[item].unshift(idInfo.toString());
   };
 
   // if file exists (and thus header too), replace header object with empty array for new rows
   const path = "trafficVolumeByLength.csv"
   if (fs.existsSync(path)){
-    let csv = [[], ...rowData ].join('\r\n');
+    let csv = [[], ...returnedRowData ].join('\r\n');
     FileWriter(path, csv,'ID ' + idInfo[0] + ' - Write row without header -->');
-    tmpRowData.length = 0;
-    rowData.length = 0;
+    // tmpRowData.length = 0;
+    // rowData.length = 0;
   }
   else {
     // Headers must be defined manually
     let manualHeaders = ['id','name','trafficRegistrationType','county','municipality', 'lat','lon','From', 'To', 'total-volume','Total-coverage', 'LengthRange:..-5.6', 'LengthRange:5.6-..','LengthRange:5.6-7.6','LengthRange:7.6-12.5','LengthRange:12.5-16','LengthRange:16-24','LengthRange:24-..']
 
     // join header and body, and break into separate rows
-    let csv = [manualHeaders, ...rowData].join('\r\n');
+    let csv = [manualHeaders, ...returnedRowData].join('\r\n');
 
     // callback(csv.length )
     FileWriter(path, csv, 'ID ' + idInfo[0] + ' - Write row with header -->');
-    tmpRowData.length = 0;
-    rowData.length = 0;
+    // tmpRowData.length = 0;
+    // rowData.length = 0;
   };
 };
 
@@ -81,4 +95,4 @@ export function forEach(items, callback) {
   }
 }
 
-export {TrafficVolumeByLengthCsv, getValues}
+export {TrafficVolumeByLengthCsv, outerFunc};
--- src/tests/trafficVolumeByLengthCsv.test.js
+++ src/tests/trafficVolumeByLengthCsv.test.js
@@ -1,275 +1,159 @@
-import { TrafficVolumeByLengthCsv, getValues, forEach } from "../components/trafficVolumeByLengthCsv";
+import { TrafficVolumeByLengthCsv, outerFunc, forEach } from "../components/trafficVolumeByLengthCsv";
 // const TrafficVolumeByLengthCsv = require('../components/trafficVolumeByLengthCsv')
 import {jest} from '@jest/globals'
 
-
 const jsonData = `{
-  "data": {
-      "trafficData": {
-          "trafficRegistrationPoint": {
-              "id": "74808V805815",
-              "name": "Bønesskogen nord",
-              "trafficRegistrationType": "VEHICLE",
-              "location": {
-                  "county": {
-                      "name": "Vestland"
-                  },
-                  "municipality": {
-                      "name": "Bergen"
-                  },
-                  "coordinates": {
-                      "latLon": {
-                          "lat": 60.331065,
-                          "lon": 5.29924
-                      }
-                  }
-              }
-          },
-          "volume": {
-              "byHour": {
-                  "edges": [
-                      {
-                        "node": {
-                          "from": "2023-02-02T00:00:00+01:00",
-                          "to": "2023-02-02T01:00:00+01:00",
-                          "total": {
-                              "volumeNumbers": {
-                                  "volume": 7
-                              },
-                              "coverage": {
-                                  "percentage": 100
-                              }
-                          },
-                          "byLengthRange": [
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 6
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 1
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 1
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              }
-                          ]
-                      }
-                  },
-                  {
-                      "node": {
-                          "from": "2023-02-02T01:00:00+01:00",
-                          "to": "2023-02-02T02:00:00+01:00",
-                          "total": {
-                              "volumeNumbers": {
-                                  "volume": 2
-                              },
-                              "coverage": {
-                                  "percentage": 100
-                              }
-                          },
-                          "byLengthRange": [
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 2
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              },
-                              {
-                                  "total": {
-                                      "volumeNumbers": {
-                                          "volume": 0
-                                      }
-                                  }
-                              }
-                          ]
-                      }
-                  }
-                      
-                  ]
-              }
-          }
-      }
-  }
-}`
+    "data": {
+        "trafficData": {
+            "trafficRegistrationPoint": {
+                "id": "74808V805815",
+                "name": "Bønesskogen nord",
+                "trafficRegistrationType": "VEHICLE",
+                "location": {
+                    "county": {
+                        "name": "Vestland"
+                    },
+                    "municipality": {
+                        "name": "Bergen"
+                    },
+                    "coordinates": {
+                        "latLon": {
+                            "lat": 60.331065,
+                            "lon": 5.29924
+                        }
+                    }
+                }
+            },
+            "volume": {
+                "byHour": {
+                    "edges": [
+                        {
+                          "node": {
+                            "from": "2023-02-02T00:00:00+01:00",
+                            "to": "2023-02-02T01:00:00+01:00",
+                            "total": {
+                                "volumeNumbers": {
+                                    "volume": 7
+                                },
+                                "coverage": {
+                                    "percentage": 100
+                                }
+                            },
+                            "byLengthRange": [
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 6
+                                        }
+                                    }
+                                },
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 1
+                                        }
+                                    }
+                                },  
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 0
+                                        }
+                                    }
+                                },
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 0
+                                        }
+                                    }
+                                },
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 1
+                                        }
+                                    }
+                                },
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 0
+                                        }
+                                    }
+                                },
+                                {
+                                    "total": {
+                                        "volumeNumbers": {
+                                            "volume": 0
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                        
+                    ]
+                }
+            }
+        }
+    }
+  }`
 
 let jsonObj = JSON.parse(jsonData);
 
-test('testing av getValues', () => {
+describe('getValues', () => {
+    let data;
+    let expectedOutput;
     let item1 = ["74808V805815", "Bønesskogen nord", "VEHICLE", "Vestland", "Bergen", "60.331065", "5.29924"]
     let item2 = ["2023-02-02T00:00:00+01:00", "2023-02-02T01:00:00+01:00", "7", "100", "6", "1", "0", "0", "1", "0","0"]
-    let actual = getValues(jsonObj)
+    let actual = outerFunc(jsonObj)
+  
+    beforeEach(() => {
+      // data = undefined;
+    //   data = jsonObj
+      expectedOutput = undefined;
+    });
+  
+    it('should handle a complete object', () => {
+        //data = getValues(jsonObj.data.trafficData.volume.byHour.edges[0])
+        expectedOutput = [item1,item2];
+  
+      expect(actual).toEqual(expectedOutput);
+    });
+  
+    it('should handle objects with byLengthRange', () => {
+      data = `{"edges": [
+        {"byLengthRange": [
+        {
+            "total": {
+                "volumeNumbers": {
+                    "volume": 6
+                }
+            }
+        }]}]}`
 
-    expect(actual).toContainEqual(item1);
-    expect(actual).toContainEqual(item2)
-    expect(actual).toEqual([item1,item2])
-  // expect(getValues(jsonObj)).toContainEqual(["74808V805815", "Bønesskogen nord", "VEHICLE", "Vestland", "Bergen", "60.331065", "5.29924" ]); // Denne fungerer
-  // expect(getValues(jsonObj)).toContainEqual(["74808V805815", "Bønesskogen nord", "VEHICLE", "Vestland", "Bergen", "60.331065", "5.29924"], ["Contains no data"]); // Denne fungerer uten 'edge' data
-//   expect(getValues(jsonObj).tmpRowData).toBe([])
-})
-
-
-// const csv = 'id,name,trafficRegistrationType,county,municipality,lat,lon,From,To,total-volume,Total-coverage,LengthRange:..-5.6,LengthRange:5.6-..,LengthRange:5.6-7.6,LengthRange:7.6-12.5,LengthRange:12.5-16,LengthRange:16-24,LengthRange:24-..\r\n' +
-// '85010V3144570,RÅDALSKRYSSET SØR RAMPE BERGEN-FANAVEGEN,VEHICLE,Vestland,Bergen,60.288265,5.32636,2023-02-02T00:00:00+01:00,2023-02-02T01:00:00+01:00,7,100,6,1,0,0,1,0,0\r\n'
-
-
-// test('Checks the final csv object', () => {
-//   expect(TrafficVolumeByLengthCsv(jsonObj)).toContainEqual('id,name,trafficRegistrationType,county,municipality,lat,lon,From,To,total-volume,Total-coverage,LengthRange:..-5.6,LengthRange:5.6-..,LengthRange:5.6-7.6,LengthRange:7.6-12.5,LengthRange:12.5-16,LengthRange:16-24,LengthRange:24-..\r\n' +
-//   '74808V805815,Bønesskogen nord,VEHICLE,Vestland,Bergen,60.331065,5.29924,2023-02-02T00:00:00+01:00,2023-02-02T01:00:00+01:00,11,100,7,4,1,0,2,1,0\r\n')
-// })
-
-
-
-
-// const myMockCallback = jest.fn()
-// console.log(mockCallback.mock.results);
-// const test1 = new myMockCallback()
-test('test TrafficVolumeByLengthCsv med Mock', () => {
-  const myMockCallback = jest.fn()
-  const test1 = new myMockCallback()
-  test1.name = 'test1'
-  console.log(myMockCallback.mock);
-
-  const test2 = [];
-  test2.name = 'test2';
-  const bound = myMockCallback.bind(test2)
-  bound()
-  console.log(myMockCallback.mock );
-  TrafficVolumeByLengthCsv(jsonObj, myMockCallback);
-  // expect(mockCallback.mock.results).toContainEqual('id,name,trafficRegistrationType,county,municipality,lat,lon,From,To,total-volume,Total-coverage,LengthRange:..-5.6,LengthRange:5.6-..,LengthRange:5.6-7.6,LengthRange:7.6-12.5,LengthRange:12.5-16,LengthRange:16-24,LengthRange:24-..\r\n' +
-  // '74808V805815,Bønesskogen nord,VEHICLE,Vestland,Bergen,60.331065,5.29924,2023-02-02T00:00:00+01:00,2023-02-02T01:00:00+01:00,11,100,7,4,1,0,2,1,0\r\n')
-  expect(myMockCallback.mock.calls).toHaveLength(2)
-})
-
-
-jest.mock("../components/trafficVolumeByLengthCsv");
-
-describe('Test TrafficVolumeByLengthCsv med mock 2', () => {
-  // beforeEach(() => {
-  //   TrafficVolumeByLengthCsv.mockClear();
-  // });
-
-  it('Sjekk dataen', () => {
-    TrafficVolumeByLengthCsv.mockImplementation(() => {
-      return {
-        Retrieve: (callback) => callback("tull og tøys")
-      }
-    })
-  })
-})
-
-
-// test('test av mock', () => {
-//   TrafficVolumeByLengthCsv (jsonObj)
-// })
-
-
-const mockCallback = jest.fn(x => 42 + x);
-
-test('forEach mock function', () => {
-  forEach([0, 1], mockCallback);
-
-  // The mock function was called twice
-  expect(mockCallback.mock.calls).toHaveLength(2);
-
-  // The first argument of the first call to the function was 0
-  expect(mockCallback.mock.calls[0][0]).toBe(0);
-
-  // The first argument of the second call to the function was 1
-  expect(mockCallback.mock.calls[1][0]).toBe(1);
-
-  // The return value of the first call to the function was 42
-  expect(mockCallback.mock.results[0].value).toBe(42);
-});
-
-
-test('this', () => {
-  const myMock1 = jest.fn();
-const a = new myMock1();
-a.name = 'a'
-console.log(myMock1.mock.instances);
-// > [ <a> ]
-
-const b = {};
-b.name = 'b'
-const bound = myMock1.bind(b);
-bound();
-console.log(myMock1.mock);
-// > [ <b> ]|
-})
-
-
- jest.mock("node-fetch")
+        let byLengthRangeData = JSON.parse(data);
+        let actual_2 = outerFunc(byLengthRangeData)
+      // data = getValues(data.data.trafficData.volume.byHour.edges[0].node.byLengthRange[0].total.volumeNumbers.volume)
+    //   expectedOutput = {byLengthRange:[1,2,3]};
+    expectedOutput = [6];
+  
+      expect(actual_2).toEqual(expectedOutput);
+    });
+  
+    it('should handle non-objects', () => {
+      data = 1;
+      expectedOutput = [['1']];
+  
+    //   expect(getValues(data)).toEqual(expectedOutput);
+    });
+  
+    it('should handle null inputs', () => {
+      data = null;
+      expectedOutput = [];
+  
+    //   expect(getValues(data)).toEqual(expectedOutput);
+    });
+  });
+  
