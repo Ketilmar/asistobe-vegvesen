0761c2f3f28c7afb992fe2d93e43957cd514b2b6
michhkh <michaelh.kodehode@gmail.com>
2023-02-28 12:35:45 +0100

    updated jsonTest to test getValues with the new query

--- src/tests/jsonTest.test.js
+++ src/tests/jsonTest.test.js
@@ -7,6 +7,10 @@ const jsonData = `{
                 "id": "74808V805815",
                 "name": "Bønesskogen nord",
                 "trafficRegistrationType": "VEHICLE",
+                "direction": {
+                    "fromAccordingToRoadLink": "Bråtet", 
+                    "toAccordingToRoadLink": "Bønes"
+                },
                 "location": {
                     "county": {
                         "name": "Vestland"
@@ -24,144 +28,198 @@ const jsonData = `{
             },
             "volume": {
                 "byHour": {
-                    "edges": [
-                        {
-                          "node": {
-                            "from": "2023-02-02T00:00:00+01:00",
-                            "to": "2023-02-02T01:00:00+01:00",
-                            "total": {
-                                "volumeNumbers": {
-                                    "volume": 7
-                                },
-                                "coverage": {
-                                    "percentage": 100
-                                }
-                            },
-                            "byLengthRange": [
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 6
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 1
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 1
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                }
-                            ]
-                        }
-                    },
+                  "edges": [
                     {
-                        "node": {
-                            "from": "2023-02-02T01:00:00+01:00",
-                            "to": "2023-02-02T02:00:00+01:00",
+                      "node": {
+                        "from": "2023-02-25T00:00:00+01:00",
+                        "to": "2023-02-25T01:00:00+01:00",
+                        "byDirection": [
+                          {
+                            "heading": "Bønes",
                             "total": {
-                                "volumeNumbers": {
-                                    "volume": 2
-                                },
-                                "coverage": {
-                                    "percentage": 100
-                                }
+                              "volumeNumbers": {
+                                "volume": 21
+                              }
                             },
                             "byLengthRange": [
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 2
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
-                                },
-                                {
-                                    "total": {
-                                        "volumeNumbers": {
-                                            "volume": 0
-                                        }
-                                    }
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 19
+                                  }
                                 }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 2
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 0
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 0
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 0
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 2
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 0
+                                  }
+                                }
+                              }
                             ]
-                        }
+                          },
+                          {
+                            "heading": "Bråtet",
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 16
+                              }
+                            },
+                            "byLengthRange": [
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 13
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 3
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 1
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 0
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 1
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 1
+                                  }
+                                }
+                              },
+                              {
+                                "total": {
+                                  "volumeNumbers": {
+                                    "volume": 0
+                                  }
+                                }
+                              }
+                            ]
+                          }
+                        ],
+                        "total": {
+                          "volumeNumbers": {
+                            "volume": 37
+                          },
+                          "coverage": {
+                            "percentage": 100
+                          }
+                        },
+                        "byLengthRange": [
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 32
+                              }
+                            }
+                          },
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 5
+                              }
+                            }
+                          },
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 1
+                              }
+                            }
+                          },
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 0
+                              }
+                            }
+                          },
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 1
+                              }
+                            }
+                          },
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 3
+                              }
+                            }
+                          },
+                          {
+                            "total": {
+                              "volumeNumbers": {
+                                "volume": 0
+                              }
+                            }
+                          }
+                        ]
+                      }
                     }
-                        
-                    ]
-                }
+                ]
             }
+          }
         }
     }
-  }`;
+}`;
 
 let jsonObj = JSON.parse(jsonData);
 
@@ -171,24 +229,45 @@ describe("CSV test", () => {
       "74808V805815",
       "Bønesskogen nord",
       "VEHICLE",
+      "Bråtet",
+      "Bønes",
       "Vestland",
       "Bergen",
       "60.331065",
       "5.29924",
     ];
     let item2 = [
-      "2023-02-02T00:00:00+01:00",
-      "2023-02-02T01:00:00+01:00",
-      "7",
+      "2023-02-25T00:00:00+01:00",
+      "2023-02-25T01:00:00+01:00",
+      "Bønes",
+      "21",
+      "19",
+      "2",
+      "0",
+      "0",
+      "0",
+      "2",
+      "0",
+      "Bråtet",
+      "16",
+      "13",
+      "3",
+      "1",
+      "0",
+      "1",
+      "1",
+      "0",
+      "37",
       "100",
-      "6",
+      "32",
+      "5",
       "1",
       "0",
-      "0",
       "1",
-      "0",
+      "3",
       "0",
     ];
+
     let actual = getValues(jsonObj);
 
     expect(actual).toContainEqual(item1); // Denne fungerer med og uten 'edge' data
