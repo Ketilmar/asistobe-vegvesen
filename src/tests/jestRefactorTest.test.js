2dc93860e6676377a8ee1d36eaf7b49a9187f455
michhkh <michaelh.kodehode@gmail.com>
2023-03-28 13:56:05 +0200

    updated code

--- src/tests/jestRefactorTest.test.js
+++ src/tests/jestRefactorTest.test.js
@@ -1,41 +1,17 @@
 import { returnRowData } from "../components/testActualRefactor.js";
 
-let exampleData = [[
-  "74808V805815",
-  "Bønesskogen nord",
-  "VEHICLE",
-  "Vestland",
-  "Bergen",
-  "60.331065",
-  "5.29924",
-],
-[
-  "2023-02-25T00:00:00+01:00,2023-02-25T01:00:00+01:00,37,100",
-  "Bønes",
-],
-[
-  "2023-02-25T00:00:00+01:00,2023-02-25T01:00:00+01:00,37,100",
-  "Bråtet",
-]];
+let exampleData = [["74808V805815","Bønesskogen nord","VEHICLE","Vestland","Bergen","60.331065","5.29924"],
+["2023-02-25T00:00:00+01:00,2023-02-25T01:00:00+01:00,37,100","Bønes"],
+["2023-02-25T00:00:00+01:00,2023-02-25T01:00:00+01:00,37,100","Bråtet"]];
 
-let expectedOutput1 = [
-  "74808V805815",
-  "Bønesskogen nord",
-  "VEHICLE",
-  "Vestland",
-  "Bergen",
-  "60.331065",
-  "5.29924",
-];
+let expectedOutput1 = ["74808V805815","Bønesskogen nord","VEHICLE","Vestland","Bergen","60.331065","5.29924"];
 
 let expectedOutput2 = [
-  [
-    "74808V805815,Bønesskogen nord,VEHICLE,Vestland,Bergen,60.331065,5.29924",
+  ["74808V805815,Bønesskogen nord,VEHICLE,Vestland,Bergen,60.331065,5.29924",
     "2023-02-25T00:00:00+01:00,2023-02-25T01:00:00+01:00,37,100",
     "Bønes",
   ],
-  [
-    "74808V805815,Bønesskogen nord,VEHICLE,Vestland,Bergen,60.331065,5.29924",
+  ["74808V805815,Bønesskogen nord,VEHICLE,Vestland,Bergen,60.331065,5.29924",
     "2023-02-25T00:00:00+01:00,2023-02-25T01:00:00+01:00,37,100",
     "Bråtet",
   ],
