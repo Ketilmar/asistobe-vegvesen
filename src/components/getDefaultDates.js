e01117ea484ac7573dcb24e1df8b95ae14f384c8
michhkh <michaelh.kodehode@gmail.com>
2023-04-18 12:55:29 +0200

    getDefaultDates now uses dayjs

--- src/components/getDefaultDates.js
+++ src/components/getDefaultDates.js
@@ -1,7 +1,13 @@
-import moment from "moment/moment.js";
+//import moment from "moment/moment.js";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js"
+import customParseFormat from 'dayjs/plugin/customParseFormat.js'
+
+dayjs.extend(customParseFormat)
+dayjs.extend(utc)
 
 const getDefaultDates = (daysToGoBack) => {
-  const date = moment().subtract(daysToGoBack, "days").format("YYYY-MM-DD");
+  const date = dayjs().subtract(daysToGoBack, "day").format("YYYY-MM-DD");
   return date;
 };
 
