2228b12fc0a250958e8032d0d9da69f11799dc6a
michhkh <michaelh.kodehode@gmail.com>
2023-04-18 15:51:17 +0200

    cleaned up code and changed some tests

--- src/tests/jestTests.test.js
+++ src/tests/jestTests.test.js
@@ -1,62 +1,53 @@
-import moment from "moment/moment";
-import { inputCheck } from "../components/fetchData";
 import { trafficVolumeByLength } from "../components/queries";
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc.js"
-import timezone from "dayjs/plugin/timezone.js" // dependent on utc plugin
-import customParseFormat from 'dayjs/plugin/customParseFormat.js'
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js"; // dependent on utc plugin
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
+import { getTimezones } from "../components/getTimezones";
+import { getDefaultDates } from "../components/getDefaultDates"
 
-describe("Date number tests", () => {
-  test("Date inputs", () => {
-    // arrange and act
-    let opposite = inputCheck("10-11-2022", "11-11-2022")
-    let incomplete = inputCheck("2022-11", "2022-11-10")
-    let emptyStringWithSpace = inputCheck(" ", " ")
-    let empty = inputCheck("", "")
-    let inputTest = inputCheck("2022-12-30", "2022-02-06");
-    let testFail = inputCheck("2022-13-30", "2022-05-05");
-    let testFormat = inputCheck("2022.12.30", "2022.12.31");
-    let testDate = "2022-02-05";
-    let dateTest = moment().subtract(1, "days").format("YYYY-MM-DD");
-    let regEx = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
-
-    //assert
-    expect(opposite).toBeFalsy()
-    expect(incomplete).toBeFalsy()
-    expect(emptyStringWithSpace).toBeFalsy()
-    expect(empty).toBeFalsy()
-    expect(inputTest).not.toBeFalsy();
-    expect(testFail).toBeFalsy();
-    expect(testFormat).toBeFalsy();
-    expect(testDate).toMatch(regEx);
-    expect(dateTest).toMatch(regEx);
-  });
-
+describe("Date tests", () => {
   test("dayjs", () => {
     //set up dayjs
-    dayjs.extend(customParseFormat)
-    dayjs.extend(utc)
-    dayjs.extend(timezone)
+    dayjs.extend(customParseFormat);
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
 
-    let testFromDate = "2023-01-01"
-    let testToDate = "2023-04-01"
+    let testFromDate = "2023-01-01";
+    let testToDate = "2023-04-01";
 
-    let testLeap = dayjs().subtract(1138, 'day').format("YYYY-MM-DD")
-    let testMonths = dayjs().subtract(11, 'day').format("YYYY-MM-DD")
-    let parsedDate = dayjs('2020-02-29', 'YYYY-MM-DD').isValid() // strict parsing
-    let fromDate = dayjs().subtract(2, 'day').format("YYYY-MM-DD")
-    let toDate = dayjs().subtract(1, 'day').format("YYYY-MM-DD")
-    let timezoneCheck1 = dayjs.tz(`${testFromDate} 00:00`, "Europe/Oslo").format();
-    let timezoneCheck2 = dayjs.tz(`${testToDate} 00:00`, "Europe/Oslo").format();
+    let parsedDate = dayjs("2021-01-01", "YYYY-MM-DD").isValid(); // strict parsing
+    let wrongFormat = dayjs("01-01-2021", "YYYY-MM-DD").isValid()
+    let actual1 = dayjs.tz(`${testFromDate} 00:00`, "Europe/Oslo").format();
+    let actual2 = dayjs.tz(`${testToDate} 00:00`, "Europe/Oslo").format();
+    let expectedOutput1 = "2023-01-01T00:00:00+01:00"
+    let expectedOutput2 = "2023-04-01T00:00:00+02:00"
 
-    expect(testLeap).toBe("2020-02-29")
-    expect(testMonths).toBe("2023-04-01")
-    expect(parsedDate).toBeTruthy()
-    expect(fromDate).toMatch("2023-04-10")
-    expect(toDate).toMatch("2023-04-11")
-    expect(timezoneCheck1).toBe("2023-01-01T00:00:00+01:00")
-    expect(timezoneCheck2).toBe("2023-04-01T00:00:00+02:00")
+    
+    expect(parsedDate).toBeTruthy();
+    expect(wrongFormat).toBeFalsy();
+    expect(actual1).toBe(expectedOutput1);
+    expect(actual2).toBe(expectedOutput2);
+  });
+
+  test("getDefaultDates", () => {
+    let regEx = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+    let actual = getDefaultDates(2)
+    
+    expect(actual).toMatch(regEx)
   })
+
+  test("getTimezones", () => {
+    let testFromDate = "2023-01-01";
+    let testToDate = "2023-04-01";
+
+    let actual = getTimezones(testFromDate, testToDate);
+    let expectedOutput1 = "2023-01-01T00:00:00+01:00";
+    let expectedOutput2 = "2023-04-01T00:00:00+02:00";
+
+    expect(actual[0]).toBe(expectedOutput1);
+    expect(actual[1]).toBe(expectedOutput2);
+  });
 });
 
 test("Query function", () => {
@@ -116,6 +107,10 @@ test("Query function", () => {
                 }
               }
             }
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
           }
         }
       }
--- src/tests/jsonTest.test.js
+++ src/tests/jsonTest.test.js
@@ -212,105 +212,10 @@ describe("Test of objectPeeler", () => {
 
   
   test("Handling id's", () => {
-    let data = {
-      data: {
-        trafficData: {
-          trafficRegistrationPoint: {
-            id: "44656V72812",
-            name: "Elgeseter gate ved Abels gate",
-            location: {
-              county: {
-                name: "Trøndelag",
-              },
-              municipality: {
-                name: "Trondheim",
-              },
-              coordinates: {
-                latLon: {
-                  lat: 63.416654,
-                  lon: 10.397405,
-                },
-              },
-            },
-          },
-          volume: {
-            byHour: {
-              edges: [
-                {
-                  node: {
-                    from: "2023-02-05T00:00:00+01:00",
-                    to: "2023-02-05T01:00:00+01:00",
-                    total: {
-                      volumeNumbers: {
-                        volume: 605,
-                      },
-                      coverage: {
-                        percentage: 100,
-                      },
-                    },
-                    byLengthRange: [
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 539,
-                          },
-                        },
-                      },
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 66,
-                          },
-                        },
-                      },
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 11,
-                          },
-                        },
-                      },
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 13,
-                          },
-                        },
-                      },
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 16,
-                          },
-                        },
-                      },
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 21,
-                          },
-                        },
-                      },
-                      {
-                        total: {
-                          volumeNumbers: {
-                            volume: 5,
-                          },
-                        },
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
-          },
-        },
-      },
-    };
-
-    let expectedOutput = objectPeeler(data);
+    let expectedOutput = objectPeeler(jsonObj);
+    
     expect(expectedOutput).toEqual(
-      expect.arrayContaining([expect.arrayContaining(["44656V72812"])])
+      expect.arrayContaining([expect.arrayContaining(["74808V805815"])])
     );
   });
 });
